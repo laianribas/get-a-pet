1f46254034d509fc8555fb145c5c026b6f5cc338
laianribas <laian67a@gmail.com>
2022-04-11 17:12:24 -0300

    aplicando react router e alguns componentes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,30 @@
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate
+} from 'react-router-dom'
+import Footer from './components/layout/Footer'
+import Navbar from './components/layout/Navbar'
+import Login from './components/pages/auth/Login'
+import Register from './components/pages/auth/Register'
+import Home from './components/pages/Home'
+
 function App() {
   return (
-    <div className="App">
-      <h1>Get A Pet</h1>
-    </div>
+    <Router>
+      <Navbar />
+      <Routes>
+        {/* <Route path="/" element="{<Home />}" />
+                                <Route path="/users" element="{<Users />}" />
+                                <Route path="/allusers" element={<Navigate to="/users" />} />
+                                <Route path="/contact" element="{<Contact />}" /> */}
+        <Route path="/" element={<Home />} />{' '}
+        <Route path="/login" element={<Login />} />{' '}
+        <Route path="/register" element={<Register />} />{' '}
+      </Routes>{' '}
+      <Footer />
+    </Router>
   )
 }
 
---
+++ frontend/src/components/layout/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import styles from './Footer.module.css'
+
+const Footer = () => {
+  return (
+    <footer className={styles.footer}>
+      <p>
+        <span className="bold">Get A Pet</span> &copy; 2021
+      </p>
+    </footer>
+  )
+}
+
+export default Footer
