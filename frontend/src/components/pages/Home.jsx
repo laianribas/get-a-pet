a52850b3552e7ea9a9892157b738d3801e944c06
laianribas <laian67a@gmail.com>
2022-04-17 11:13:31 -0300

    edição de pet e estrutura da home

--- frontend/src/components/pages/Home.jsx
+++ frontend/src/components/pages/Home.jsx
@@ -1,6 +1,16 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import api from '../../utils/api'
+import { Link } from 'react-router-dom'
+import styles from './Home.module.css'
 
 const Home = () => {
+  const [pets, setPets] = useState([])
+
+  useEffect(() => {
+    api.get('/pets').then((response) => {
+      setPets(response.data.pets)
+    })
+  }, [pets])
   return (
     <section>
       <h1>Home</h1>
